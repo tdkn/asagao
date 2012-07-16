587a8d2d63adf6bac15a793fde3d095ca428a9cb
tdkn <web@tedokon.com>
2012-07-17 02:24:07 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## About
+"改訂新版 基礎Ruby on Rails" の勉強用に作られたレポジトリです。
+
+## Links
+http://www.impressjapan.jp/books/3156
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asagao::Application.config.secret_token = '1d0c7315eafb4d99b0307c0f60a84e5c1f4133a8b457d2cba008ccbb3e42b31d2cedbe5754b7938e880576b8d0402ad309d61c13454e638a55da9edefccd397e'
